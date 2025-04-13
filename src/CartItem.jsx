83d1fcb281f174a3d80bba2f8aa323907973ddd9
Deniz <auo@tuta.io>
2025-04-13 15:36:59 +0200

    handle checkout

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -24,7 +24,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleCheckoutShopping = (e) => {
-    alert('Functionality to be added for future reference');
+    alert('To be added soon');
   };
 
   const handleIncrement = (item) => {
@@ -75,7 +75,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
